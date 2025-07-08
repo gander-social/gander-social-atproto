23ff6b17416850c25e624f2aff87a83e3ab66e8f
Melody Lumen <melody@mrldigitalstudio.com>
2025-07-08 14:48:35 -0300

    :recycle: Refactor BskyAgent to GndrAgent and update related code references

--- packages/api/src/bsky-agent.ts
+++ packages/api/src/bsky-agent.ts
@@ -1,14 +1,14 @@
 import { AtpAgent } from './atp-agent'
 
 /** @deprecated use {@link AtpAgent} instead */
-export class BskyAgent extends AtpAgent {
+export class GndrAgent extends AtpAgent {
   clone(): this {
-    if (this.constructor === BskyAgent) {
-      const agent = new BskyAgent(this.sessionManager)
+    if (this.constructor === GndrAgent) {
+      const agent = new GndrAgent(this.sessionManager)
       return this.copyInto(agent as this)
     }
 
     // sub-classes should override this method
-    throw new TypeError('Cannot clone a subclass of BskyAgent')
+    throw new TypeError('Cannot clone a subclass of GndrAgent')
   }
 }
